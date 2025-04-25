efb30408e0f0d58a7315ac07900c9ccfe5ffc012
luchitodmj <lu.montecinosj@duocuc.cl>
2025-04-25 10:56:07 -0400

    changes done in serializers urls ans views

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CategoriaViewSet, consumir_api_externa, consumir_api_externa_html
+from .views import (
+    CategoriaViewSet, 
+    ProductoViewSet,
+    MarcaViewSet,
+    TipoProdViewSet,
+    consumir_api_externa, 
+    consumir_api_externa_html
+)
 
 # Rutas automáticas del ViewSet
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
+router.register(r'productos', ProductoViewSet)
+router.register(r'marcas', MarcaViewSet)
+router.register(r'tipos-producto', TipoProdViewSet)
 
 # Rutas adicionales manuales
 extra_urlpatterns = [
